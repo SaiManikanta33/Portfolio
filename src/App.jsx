c1a6170a6d1b7779613d822f3dd86b026b7a526a
SaiManikanta <saimanikantam018@gmail.com>
2026-05-29 09:52:56 +0530

    Removed particle background

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home";
 import Footer from "./Components/Footer";
-import ParticleBackground from "./Components/ParticleBackground";
 function App() {
   return (
     <>
-      <ParticleBackground />
       <Navbar />
       <Home />
       <Footer />
