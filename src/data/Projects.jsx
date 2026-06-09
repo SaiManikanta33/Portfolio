0e8562f5aaeed54929a697e6df3b44c6501ccec9
SaiManikanta <saimanikantam018@gmail.com>
2026-06-09 18:32:43 +0530

    Added project image

--- src/data/Projects.jsx
+++ src/data/Projects.jsx
@@ -3,41 +3,45 @@ const projects = [
   {
     id: 1,
     title: "Personal Portfolio Website",
+    image: "",
     description:
       "A responsive portfolio website showcasing my projects and skills.",
     tech: "React , JavaScript , CSS , HTML",
     github: "https://github.com/SaiManikanta33/Portfolio",
-    demo: "https://saimanikanta33.github.io/Portfolio/",
+    demo: "https://portfolio-fmrn.vercel.app/",
   },
 
   {
     id: 2,
     title: "Port Scanner",
+    image: "PortScanner.png",
     description:
       "Python based tool to scan open ports in a network.",
     tech: "Python Socket Programming",
-    github: "#",
-    demo: "#",
+    github: "https://github.com/SaiManikanta33/Port-Scanner",
+    demo: "https://port-scanner.vercel.app/",
   },
 
   {
     id: 3,
     title: "Cyber Security Portfolio",
+    image: "CyberSecurityPortfolio.png",
     description:
       "Personal responsive portfolio website using React.",
     tech: "React , JavaScript , CSS , HTML",
-    github: "#",
-    demo: "#",
+    github: "https://github.com/SaiManikanta33/Cyber-Security-Portfolio",
+    demo: "https://cyber-security-portfolio.vercel.app/",
   },
 
   {
     id: 4,
     title: "Linux Commands Website",
+    image: "LinuxCommandsWebsite.png",
     description:
       "Website for learning important Linux commands.",
     tech: "MERN Stack",
-    github: "#",
-    demo: "#",
+    github: "https://github.com/SaiManikanta33/Linux-Commands-Website",
+    demo: "https://linux-commands-website.vercel.app/",
   },
 
 ];
